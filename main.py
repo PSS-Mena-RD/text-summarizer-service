e3d2b34e00a8547c5b430f3f1b85bae23cda6e54
Marwa Essam <marwa.essam.pss@gmail.com>
2025-10-27 20:46:44 +0300

    update the main method

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 app = FastAPI()
 
 @app.post("/summarize")
-async def summarize(file: UploadFile, api_key: str = Form(...)):
+async def summarize(file: UploadFile):
     """
     Receives a text document and returns a summary.
     """
@@ -13,6 +13,10 @@ async def summarize(file: UploadFile, api_key: str = Form(...)):
     text = content.decode("utf-8")
     genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
     model = genai.GenerativeModel("gemini-2.5-flash")
-    response = model.generate_content(f"Summarize the following Arabic text in no more than 250 words:\n{text}")
+    response = model.generate_content(f"I will give you a document written in either Arabic, English, or a mix of both . Summarize the document in its most used language in no more than 250 words:\n{text}")
     summary = response.text
     return JSONResponse({"summary": summary})
+
+@app.get("/")
+def read_root():
+    return {"message": "Service is running!"}
