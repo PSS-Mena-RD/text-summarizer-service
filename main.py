840f8766125a01e750d08cebd9014864d01f1d1b
Marwa Essam <marwa.essam.pss@gmail.com>
2025-11-06 11:48:01 +0200

    Input the document as a text

--- main.py
+++ main.py
@@ -35,7 +35,9 @@ async def summarize(input: TextIn):
     content=input.text
     genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
     model = genai.GenerativeModel("gemini-2.5-flash")
-    response = model.generate_content(f"I will give you a text written in either Arabic, English, or a mix of both . Summarize the document in its most used language in no more than 250 words. Provide the summary as in JSON with the field Text_Summary: \n{text}")
+    response = model.generate_content(f"I will give you a text written in either Arabic, English, or a mix of both."
+                                      f"Summarize the document in its most used language in no more than 250 words."
+                                      f"Provide the summary as in JSON with the field Text_Summary: \n{content}")
     summary = response.text
     return JSONResponse({"Text_Summary": summary})
 @app.get("/")
